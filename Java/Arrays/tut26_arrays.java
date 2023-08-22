56ad8b88ed53728ac7ebd00c8c8a86de63620a2f
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-22 23:17:38 +0530

    tut 26 edit completed now tut 27 empty

--- Java/Arrays/tut26_arrays.java
+++ Java/Arrays/tut26_arrays.java
@@ -1,6 +1,7 @@
 public class tut26_arrays {
     public static void main(String[] args) {
-        
+        // int[] marks = new int[5];
+        int[] marks={100, 200, 300, 400, 500};
     }
-    
+
 }
