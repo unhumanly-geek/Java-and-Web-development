59f383f2297becf5c0a203d20f742599aedfbffb
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 02:05:31 +0530

    Matrices2 added

---
+++ Java/Arrays/PracticeSet/Matrices2.java
@@ -0,0 +1,18 @@
+package PracticeSet;
+
+import java.util.*;
+
+public class Matrices2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter for x axis: ");
+        int xaxis = sc.nextInt();
+
+        System.out.print("Enter for y axis: ");
+        int yaxis = sc.nextInt();
+
+        int[][] arr = new int[xaxis][yaxis];
+
+    }
+}
