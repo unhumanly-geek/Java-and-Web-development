5d3a6f8705286c934289b535d4005aea995a338b
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 18:03:13 +0530

    Double triple done

---
+++ Java/Strings/Practice_Set/Double_Triple_Spaces.java
@@ -0,0 +1,11 @@
+package Strings.Practice_Set;
+
+public class Double_Triple_Spaces {
+    public static void main(String[] args) {
+        String k = "This is a  double and Triple   spaces scentence";
+
+        System.out.println(k.indexOf("  "));
+        System.out.println(k.indexOf("   "));
+    }
+    
+}
