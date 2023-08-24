b874546e7f792fb552eb1029a3e6fd1c1cdbe833
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 23:53:19 +0530

    recursion added

---
+++ Java/Methods_Recursion/Recursion.java
@@ -0,0 +1,18 @@
+package Methods_Recursion;
+
+public class Recursion {
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int x = 4;
+        System.out.println("Value of factorial x is: " + factorial(x));
+    }
+
+}
