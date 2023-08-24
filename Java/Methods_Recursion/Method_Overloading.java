4b47c81295e2d0e5338499fed91726802da7e2bf
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 23:02:09 +0530

    method overloading done

--- Java/Methods_Recursion/Method_Overloading.java
+++ Java/Methods_Recursion/Method_Overloading.java
@@ -1,5 +1,29 @@
 package Methods_Recursion;
 
 public class Method_Overloading {
+    static void data(int x, int y, int z){
+        System.out.println(x+y+z);
+
+    }
+    static void data(int x, int y){
+        System.out.println(x+y);
+    }
+    static void data(int x){
+        System.out.println(x);
+    }
+    static void data(){
+        System.out.println("Enter Valid Data");
+    }
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        data(a, b, c);
+        data(b, c);
+        data(c);
+        data();
+    }
     
 }
