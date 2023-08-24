63be56fb0c7c1f7f7923d0def37c27b148e31b93
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 18:07:58 +0530

    Escape_Sequence file done

---
+++ Java/Strings/Practice_Set/Escape_Sequence.java
@@ -0,0 +1,8 @@
+package Strings.Practice_Set;
+
+public class Escape_Sequence {
+    public static void main(String[] args) {
+        System.out.println("Dear Harry, \n\tThis java course is nice.\n Thanks");
+    }
+    
+}
