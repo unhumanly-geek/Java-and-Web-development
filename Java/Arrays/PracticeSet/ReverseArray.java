caf754a3b4583c1120bf5e71f7c1220e0bc1b97d
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 14:27:52 +0530

    revarr added

--- Java/Arrays/PracticeSet/ReverseArray.java
+++ Java/Arrays/PracticeSet/ReverseArray.java
@@ -6,9 +6,25 @@ public class ReverseArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println();
+        System.out.println("Enter Length: ");
+
         int len = sc.nextInt();
         int[] arr = new int[len];
+
+        System.out.println("Enter elements: ");
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] revArr = new int[len];
+
+        for (int i = 0; i < len; i++) {
+            revArr[i] = arr[len - i - 1];
+        }
+
+        for (int i = 0; i < len; i++) {
+            System.out.println(revArr[i]);
+        }
 
     }
 
