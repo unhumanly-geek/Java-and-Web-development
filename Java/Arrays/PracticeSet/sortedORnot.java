2cd2150fa80209cd502158e5bf0992d45d0ad393
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 15:17:23 +0530

    sortedORnot written

--- Java/Arrays/PracticeSet/sortedORnot.java
+++ Java/Arrays/PracticeSet/sortedORnot.java
@@ -1,5 +1,21 @@
 package PracticeSet;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class sortedORnot {
+    public static void main(String[] args) {
         
+        Scanner sc = new Scanner(System.in);
+
+        int len = sc.nextInt();
+        int [] arr = new int[len];
+        int [] arr2 = (int[]) arr.clone();
+
+        for(int i = 0; i<len; i++){
+            arr[len]=sc.nextInt();
+        }
+
+    }
+
 }
