34d47e4001f1f155b1580a03b102a2acbd2b5edc
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 16:48:24 +0530

    tut13 intro

---
+++ Java/Strings/tut13_Intro.java
@@ -0,0 +1,23 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class tut13_Intro {
+    public static void main(String[] args) {
+        String name = "Akshat Gulati";
+
+        // String name = "Harry";
+        // System.out.print("The name is: ");
+        // System.out.print(name);
+        int a = 6;
+        float b = 5.6454f;
+        System.out.printf("The value of a is %d and value of b is %8.2f", a, b);
+        // System.out.format("The value of a is %d and value of b is %f", a, b);
+        Scanner sc = new Scanner(System.in);
+        // String st = sc.next();
+        // String st = sc.nextLine();
+        // System.out.println(st);
+
+    }
+
+}
