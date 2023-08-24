5b7a6dc50e236cd07bc715da4c3bf18161b7ff29
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 19:35:04 +0530

    tut 31 completed

--- Java/Methods_Recursion/tut31_Intro.java
+++ Java/Methods_Recursion/tut31_Intro.java
@@ -1,5 +1,21 @@
 package Methods_Recursion;
 
+import java.util.Scanner;
+
 public class tut31_Intro {
+    static int logic(int x, int y){
+         return x+y;
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int k = sc.nextInt();
+        int x = sc.nextInt();
+
+        int c = logic(k, x);
+
+        System.out.println(c);
+    }
     
 }
