3c19b3f91901ff6fa155715cfca5d5e4b8a85e0e
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-24 15:24:10 +0530

    sorted program completed

--- Java/Arrays/PracticeSet/sortedORnot.java
+++ Java/Arrays/PracticeSet/sortedORnot.java
@@ -5,15 +5,32 @@ import java.util.Scanner;
 
 public class sortedORnot {
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter Len: ");
         int len = sc.nextInt();
-        int [] arr = new int[len];
-        int [] arr2 = (int[]) arr.clone();
+        int[] arr = new int[len];
 
-        for(int i = 0; i<len; i++){
-            arr[len]=sc.nextInt();
+        for (int i = 0; i < len; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int[] arr2 = (int[]) arr.clone();
+        Arrays.sort(arr2);
+
+        boolean flag = true;
+        for (int i = 0; i < len; i++) {
+            if (arr[i] != arr2[i]) {
+                flag = false;
+
+            }
+        }
+        if (flag=true) {
+            System.out.println("Array is sorted");
+            
+        }
+        else{
+            System.out.println("Unsorted");
         }
 
     }
