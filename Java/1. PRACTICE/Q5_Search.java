713903d08da6cf666e65384c3c2eed8952003d5f
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-25 01:24:39 +0530

    Q5 Work in progress

--- Java/1. PRACTICE/Q5_Search.java
+++ Java/1. PRACTICE/Q5_Search.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
+
 public class Q5_Search {
     
-    static void search()
+    static void search(int[][] arr, int x){
+
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        
+
+
+
+    }
 }
