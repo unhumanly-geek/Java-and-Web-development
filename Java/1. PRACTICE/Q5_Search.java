a06423ec735743cb1e742013e58b37c7ad25bef0
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-25 01:41:19 +0530

    q5 done

--- Java/1. PRACTICE/Q5_Search.java
+++ Java/1. PRACTICE/Q5_Search.java
@@ -1,19 +1,48 @@
 import java.util.Scanner;
 
 public class Q5_Search {
-    
-    static void search(int[][] arr, int x){
+
+    static void search(int[][] arr, int x, int y, int n) {
+
+        boolean flag = false;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                if (n == arr[i][j]) {
+                    flag = true;
+
+                }
+            }
+        }
+        if (flag) {
+            System.out.println("Element is here");
+
+        }
+        else{
+            System.out.println("not found");
+        }
 
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter for x Axis");
         int x = sc.nextInt();
+        System.out.println("Enter for y Axis");
         int y = sc.nextInt();
+        System.out.println("Enter to find");
+        int n = sc.nextInt();
 
-        
+        int[][] arr = new int[x][y];
 
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
 
+        search(arr, x, y, n);
 
     }
 }
