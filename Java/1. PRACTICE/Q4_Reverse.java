7d5ceb5895fa75ac32cc6c440aab0edf0acaba79
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-25 01:06:30 +0530

    reverse work in progress

--- Java/1. PRACTICE/Q4_Reverse.java
+++ Java/1. PRACTICE/Q4_Reverse.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class Q4_Reverse {
 
     public static void RevArr(int[] arr, int x){
-        
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Length: ");
+        int x = sc.nextInt();
+
+        int[] arr = new int[x];
+
+        for (int i = 0; i < x; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        RevArr(arr, x);
+
     }
 
 
