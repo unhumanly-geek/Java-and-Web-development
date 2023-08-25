30038d84b72a6b004cca4c7239242ffb7faad2be
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-25 17:44:09 +0530

    Q19 done

--- Java/1. PRACTICE/Q19_Duplicates.java
+++ Java/1. PRACTICE/Q19_Duplicates.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 
 public class Q19_Duplicates {
 
-    static void duplicate(int[] arr, int k){
+    static void duplicate(int[] arr, int k) {
+
+        for (int i = 0; i < k; i++) {
+            for (int j = i+1; j < k; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate found: " + arr[j]);
+
+                }
+            }
+        }
 
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int x=sc.nextInt();
         System.out.print("Enter Length: ");
+        int x = sc.nextInt();
 
+        int[] arr = new int[x];
 
-        int []arr= new int[x]; 
-        
-        for(int i =0; i <x; i++){
-            System.out.print("Enter elements for index "+i+": ");
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < x; i++) {
+            System.out.print("Enter elements for index " + i + ": ");
+            arr[i] = sc.nextInt();
 
         }
         duplicate(arr, x);
     }
-    
+
 }
