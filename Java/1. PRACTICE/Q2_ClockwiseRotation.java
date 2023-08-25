875f81fe43549c846725208a6ce910ed516d0caa
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-08-26 00:38:32 +0530

    bugs solved

--- Java/1. PRACTICE/Q2_ClockwiseRotation.java
+++ Java/1. PRACTICE/Q2_ClockwiseRotation.java
@@ -1,17 +1,22 @@
 import java.util.Scanner;
 
 public class Q2_ClockwiseRotation {
-    static void ClockwiseRotation(int[] arr, int x, int r) {
-        int[] temp = new int [x];
-
-        for(int i =0; i<x; i++){
-            temp[i]=arr[x-i+1];
+    static void ClockwiseRotation(int[] arr, int n, int d) {
+        // create a new array to store the rotated elements
+        int[] rotated = new int[n];
+        // loop through the original array
+        for (int i = 0; i < n; i++) {
+            // calculate the new index for each element after rotation
+            // use modulo operator to wrap around the array indices
+            int newIndex = (i + d) % n;
+            // copy the element to the new index in the rotated array
+            rotated[newIndex] = arr[i];
         }
+        // return the rotated array
+        for (int i = 0; i < n; i++) {
+            System.out.println(rotated[i]);
 
-
-
-
-    }
+    }}
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
