2417bd4d5df92083f0068e0f8ade615e4af742bc
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-04 00:38:36 +0530

    08 problem 3 done

--- js_advanced/08_practice.js
+++ js_advanced/08_practice.js
@@ -1,14 +1,24 @@
-// problem-2
+// // problem-2
 
-let age = prompt("What is your age: ");
-switch (age) {
-    case 12:
-        console.log("your age is 12");
-        break;
-    case 18:
-        console.log("your age is 18");
-        break;    
+// let age = prompt("What is your age: ");
+// switch (age) {
+//     case 12:
+//         console.log("your age is 12");
+//         break;
+//     case 18:
+//         console.log("your age is 18");
+//         break;    
 
-    default:
-        break;
+//     default:
+//         break;
+// }
+
+// problem-3
+
+let a = prompt("Enter the number: ")
+if (a%2) {
+    console.log("divisible by 2");
+}
+else{
+    console.log("divisible by 3");
 }
