cbc8a4f224e5e0b7e7c3602ab89d0d0609e90fac
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-04 00:32:23 +0530

    08 problem 2 done

---
+++ js_advanced/08_practice.js
@@ -0,0 +1,14 @@
+// problem-2
+
+let age = prompt("What is your age: ");
+switch (age) {
+    case 12:
+        console.log("your age is 12");
+        break;
+    case 18:
+        console.log("your age is 18");
+        break;    
+
+    default:
+        break;
+}
