736a52acc8ab0d7203d4d40b854781b4d40b00e1
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-04 01:54:13 +0530

    12 problem1 solved

---
+++ js_advanced/12_practice.js
@@ -0,0 +1,11 @@
+const students = {}
+let a = prompt("Total no of enteries: ")
+
+for(let i =0; i<a; i++){
+    let b = prompt("name of students: ");
+    let c = prompt("marks of the : ")
+    
+    students[b]=c;
+}
+console.log(students);
+
