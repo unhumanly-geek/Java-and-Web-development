514c661c48f9fb882aade798c9aa0c7e0bddd5ec
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-05 18:47:10 +0530

    14_StringMethods.js done

---
+++ js_advanced/14_StringMethods.js
@@ -0,0 +1,15 @@
+let name = "Akshat";
+console.log(name +" :and: "+name.length);
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2))
+console.log(name.replace("a","y"))
+console.log(name.concat(" is a bad"," boy"));
+
+let name1 = "      ashu    "
+console.log(name1.trim());
+console.log(name1.includes(name))
+console.log(name1.endsWith("u"));
+console.log(name1.startsWith(" "));
