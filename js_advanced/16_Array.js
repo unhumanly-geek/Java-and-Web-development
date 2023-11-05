4eec6e828cb9583b33e1bdda9ddeaf21ccdd7cbd
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 04:36:06 +0530

    16_Array.js done

---
+++ js_advanced/16_Array.js
@@ -0,0 +1,15 @@
+let num = [1,2,34,3,4]
+let b = num.toString;
+let c = num.join("_")
+console.log(c); // typeof string
+let d = num.pop()
+console.log(d);
+console.log(num);
+let e = num.push(56)
+let f = num.push(89)
+console.log(num,e,f);
+let g = num.shift()
+console.log(g);
+console.log(num);
+let h = num.unshift(54)
+console.log(num,h)
