9df560b9c39b10cee2ae45127219448b3ab9ec73
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 08:50:11 +0530

    22_exercise.js fianlly done

--- js_advanced/22_exercise.js
+++ js_advanced/22_exercise.js
@@ -1,19 +1,19 @@
-while (true) {
-    let value = Math.floor(Math.random() * 100);
-    console.log(value)
-    let num = prompt("enter the number");
-    num = Number.parseInt(num);
+let x = Math.floor((Math.random()*100)+1)
+let chances = 0;
+let input = 0
+while(input != x){
+  input = Number.parseInt(prompt("Enter number between 0 to 100"))
 
-    if (num == value) {
+  if(input > x){
+    console.log("The number is smaller than " +input)
+  }
 
-        console.log("you enter the right value")
-        break;
-    }
-    else if (num < value) {
-        console.log("you enter the less value");
-    }
-    else if (num > value) {
-        console.log("you enter the greater value");
-    }
+  else if(input < x){
+    console.log("The number is bigger than " +input)
+  }
 
+  chances++
 }
+
+console.log("Congrats Champ")
+console.log("Your score is " +(100 - chances)+ " and number is " +x)
