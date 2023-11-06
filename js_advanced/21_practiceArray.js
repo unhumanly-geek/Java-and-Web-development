f341f056bb029ef9303dda4b266579d8ae6f1f30
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 07:28:25 +0530

    21_practiceArray.js problem 3 done

--- js_advanced/21_practiceArray.js
+++ js_advanced/21_practiceArray.js
@@ -1,10 +1,19 @@
-let n = prompt("Enter total numbers: ")
-let arr = []
-for (let i = 0; i < n; i++) {
-    let k = prompt("Enter values")
-    k = Number.parseInt(k)
-    if (k != 0) {
-        arr.push(k)
+// let n = prompt("Enter total numbers: ")
+// let arr = []
+// for (let i = 0; i < n; i++) {
+//     let k = prompt("Enter values")
+//     k = Number.parseInt(k)
+//     if (k != 0) {
+//         arr.push(k)
+//     }
+// }
+// console.log(arr);
+//  problem 3
+
+let arr = [10,20,33,44,50,70,66]
+let a1  = newarr.filter((a)=>{
+    if (a%10==0) {
+        return a
     }
-}
-console.log(arr);
+})
+console.log(a1);
