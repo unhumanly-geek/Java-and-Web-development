571d9fde30e612ef77e354b6c86abc056cf0c593
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 06:33:20 +0530

    20_map_filter_reduce.js filter done

--- js_advanced/20_map_filter_reduce.js
+++ js_advanced/20_map_filter_reduce.js
@@ -6,3 +6,10 @@ let a = arr.map((value, index, Array)=>{
     return value+1
 })
 console.log(a);
+
+// filter method
+let arr2 = [45, 23, 21, 0, 3, 5]
+let a2 = arr2.filter((a)=>{
+    return a<10
+})
+console.log(a2);
