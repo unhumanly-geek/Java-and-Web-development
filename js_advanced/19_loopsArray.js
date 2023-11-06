d923b9057be996a79b5e02053859b026878f6d4a
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 06:15:07 +0530

    19 forEach done

---
+++ js_advanced/19_loopsArray.js
@@ -0,0 +1,10 @@
+let num = [1,2,3,4,5,6,7,8]
+
+// for each loop
+num.forEach((element)=>{
+    console.log(element);
+})
+// Array.from
+let name = "Akshat"
+let arr = Array.from(name)
+console.log(arr);
