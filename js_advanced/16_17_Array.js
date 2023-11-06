4c1e007cb0d50adfb6826556f7690a091e19c0cd
Akshat Gulati <akshat0126.be21@chitkara.edu.in>
2023-11-06 05:55:08 +0530

    16_17_Array.js done

--- js_advanced/16_17_Array.js
+++ js_advanced/16_17_Array.js
@@ -13,3 +13,27 @@ console.log(g);
 console.log(num);
 let h = num.unshift(54)
 console.log(num,h)
+
+// l-18
+let n= [11,12,13,14,15,16,17,18]
+delete n[0]
+console.log(n.length, n);
+let newarr = num.concat(n,num)
+console.log(newarr);
+newarr.sort()
+console.log(newarr);
+
+
+let compare = (a, b)=>{
+    return a - b
+}
+let num2 = [14,58,35,10,175,884,288,2]
+num2.sort(compare)
+console.log("new arr: ",num2);
+num2.reverse()
+console.log(num2);
+
+// splice and slice
+let num3 = [551, 22,3,14, 5, 6, 7, 8, 229]
+let deletedvalues = num3.splice(2,3,1021,1022,1023)
+console.log(num3, deletedvalues);
